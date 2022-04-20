963b52f28e7e44e2e6f22ce95ca468f71caef2b2
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2022-04-21 00:48:17 +0600

    updated

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,7 +1,16 @@
 import React, { useState } from "react";
-import styledComponents from "styled-components";
+import styled from "styled-components";
+import { AiOutlineCaretDown } from "react-icons/ai";
+import { MdPowerSettingsNew } from "react-icons/md";
+import { GiSpeaker, GiSpeakerOff } from "react-icons/gi";
+import { FiWifi, FiWifiOff } from "react-icons/fi";
+import Close from "../components/Close";
 
 export default function Header() {
+  const [Sound, setSound] = useState(true);
+  const [HeaderDrop, setHeaderDrop] = useState(true);
+  const [close, setClose] = useState(false);
+
   const [DATE, setDATE] = useState();
   setInterval(() => {
     const date = new Date();
@@ -21,28 +30,90 @@ export default function Header() {
           10: "Nov",
           11: "Dec",
         }[date.getMonth()]
-      } ${date.getHour()}:${date.getMinutes()}`
+      } ${date.getHours()}:${date.getMinutes()}`
     );
 
     // console.log(DATE);
   }, 1000);
   return (
-    <Container>
-      <div className="elements">Activities</div>
-      <div className="elements">{DATE}</div>
-      <div className="elements"></div>
-    </Container>
+    <>
+      <Container>
+        <div className="elements">Activities</div>
+        <div className="elements">{DATE}</div>
+        <div className="elements" onClick={() => setHeaderDrop(!HeaderDrop)}>
+          <span>{navigator.onLine ? <FiWifi /> : <FiWifiOff />}</span>
+          <span>{Sound ? <GiSpeaker /> : <GiSpeakerOff />}</span>
+          <span>
+            <MdPowerSettingsNew />
+          </span>
+          <span>
+            <AiOutlineCaretDown />
+          </span>
+          <div className={`elements_drop ${HeaderDrop ? "active" : ""}`}>
+            <div className="elements_drop__element">
+              {navigator.onLine ? <FiWifi /> : <FiWifiOff />}
+              {navigator.onLine ? "You are Connected" : "No Internet"}
+            </div>
+            <div className="elements_drop__element"><span onClick={()=>setSound(!Sound)}>{Sound ? <GiSpeaker /> : <GiSpeakerOff />}</span><input type="range" name="sound" id="sound" min="0" max="10" step="1" value={Sound ? "10": "0"} /></div>
+            <div className="elements_drop__element"></div>
+            <div className="elements_drop__element"></div>
+          </div>
+        </div>
+      </Container>
+      {close === true && <Close setClose={setClose} />}
+    </>
   );
 }
 
-const Container = styledComponents.header`
-background-color: var(--nav-bg);
-height:27px;
-color: var(--nav-text);
-display: flex;
-align-items: center;
-justify-content: space-between;
-padding-left: 10px;
-padding-right: 10px;
-font-size: 14px;
+const Container = styled.header`
+  background-color: var(--nav-bg);
+  height: 27px;
+  color: var(--color-p);
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding-left: 10px;
+  padding-right: 10px;
+  font-size: var(--p-font);
+  @media (min-width: 1441px) {
+    height: calc(27px + 2vmin);
+  }
+  .elements {
+    &:nth-child(3) {
+      display: flex;
+      gap: 8px;
+      position: relative;
+      span {
+        cursor: pointer;
+      }
+    }
+    &_drop {
+      display: none;
+      flex-direction: column;
+      position: absolute;
+      right: 0;
+      top: 170%;
+      background-color: var(--color-p);
+      // padding: 10px 0;
+      width: min(250px, 85vw);
+      color: var(--nav-bg);
+      border-radius: 4px;
+      &__element {
+        padding: 10px;
+        display: flex;
+        align-items: center;
+        gap: 16px;
+        &:not(:last-child) {
+          border-bottom: 1px solid var(--color-p-dark);
+        }
+        input {
+          flex: 1;
+          accent-color: var(--terminal-body);
+        }
+      }
+      &.active {
+        display: flex;
+      }
+    }
+  }
 `;
--- src/styles/home.js
+++ src/styles/home.js
@@ -1,18 +1,21 @@
 import styled from "styled-components";
 
 const HomeContainer = styled.main`
-  max-width: 1440px;
+  /* max-width: 1440px; */
   margin: 0 auto;
   .body {
     background-image: url(/bg/default.jpg);
     background-size: cover;
     background-repeat: no-repeat;
     background-position: top;
-    min-height: calc(100vh - 27px);
+    height: calc(100vh - 27px);
     aside {
       background-color: var(--nav-bg-transparent);
       height: calc(100vh - 27px);
       width: 50px;
+      @media (min-width: 1441px) {
+        width: calc(50px + 2vmin);
+      }
     }
   }
 `;
