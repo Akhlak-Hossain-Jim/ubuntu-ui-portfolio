23f9e6bbad8f01258ccd458585045cf1bea2be02
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2022-08-18 16:24:00 +0600

    📕🛠️basics and terminal added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./layout/Header";
 import HomeContainer from "./styles/home";
+import Terminal from "./components/Terminal";
+import Aside from "./layout/Aside";
 
 function App() {
+  const [currentApp, setCurrentApp] = useState("");
+  const [showTerminal, setShowTerminal] = useState(true);
+
   return (
     <HomeContainer>
-      <Header />
+      <Header isCurrent={currentApp} setIsCurrent={setCurrentApp} />
       <div className="body">
-        <aside></aside>
+        <Aside
+          terminal={showTerminal}
+          setTerminal={setShowTerminal}
+          setCurrentApp={setCurrentApp}
+          currentApp={currentApp}
+        />
+        <main>
+          <Terminal
+            showState={showTerminal}
+            showApp={setShowTerminal}
+            isCurrent={currentApp}
+            setIsCurrent={setCurrentApp}
+          />
+        </main>
       </div>
     </HomeContainer>
   );
