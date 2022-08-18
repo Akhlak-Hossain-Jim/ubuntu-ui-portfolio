06b83e502bbb5fee244fec76429ef8d269674516
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2022-08-18 20:12:40 +0600

    💅added & updated

--- src/styles/home.js
+++ src/styles/home.js
@@ -16,6 +16,16 @@ const HomeContainer = styled.main`
       position: relative;
       height: 100%;
       overflow: hidden;
+      & > .bg {
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        z-index: 1;
+      }
     }
   }
 `;
